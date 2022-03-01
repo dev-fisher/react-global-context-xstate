487c30ab13ddb95407c52231ca1b60fbecc8733f
Fisher <fish2112fish@gmail.com>
2022-03-01 09:12:00 +0200

    added test for button functionality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,34 @@
-import { describe, expect, it } from 'vitest';
-import App from './App';
-import { fireEvent, render, screen, userEvent } from '../test-utils';
+import { describe, expect, it } from "vitest"
+import App from "./App";
+import { fireEvent, render, screen, userEvent } from "../test-utils";
 
-describe('App', () => {
-	it('Check that all the elements are on the screen', () => {
-		render(<App />);
-		const activateButton = screen.getByText('ACTIVATE');
-		expect(activateButton).toBeInTheDocument();
-		const disableButton = screen.getByText('DISABLE');
-		expect(disableButton).toBeInTheDocument();
-		const counter = screen.getByTestId('counter');
-		expect(counter).toBeInTheDocument();
-	});
-});
+
+describe("App", () => {
+    it("Check that all the elements are on the screen", () => {
+        render(<App />)
+        const activateButton = screen.getByText('ACTIVATE')
+        expect(activateButton).toBeInTheDocument()
+        const disableButton = screen.getByText('DISABLE')
+        expect(disableButton).toBeInTheDocument()
+        const counter = screen.getByTestId('counter')
+        expect(counter).toBeInTheDocument()
+    })
+
+    it("Check clicking the ACTIVATE button works", () => {
+        render(<App />)
+        const counter = screen.getByTestId('counter')
+        const activateButton = screen.getByText('ACTIVATE')
+
+        fireEvent(
+            activateButton,
+            new MouseEvent('click', {
+              bubbles: true,
+              cancelable: true,
+            }),   
+        )
+        
+        expect(counter).toHaveTextContent('2')
+        expect(counter).toHaveStyle('background: green')
+    })
+})
+
